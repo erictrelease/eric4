676aa3f1d04acb0acd50cea2dc5c82af961cdd5f
erictrelease <erictrelease@gmail.com>
2024-03-02 12:20:08 -0500

    march 2 2

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
 import streamlit as st
 import pandas as pd
-import plotly.express
-
-
-df = pd.read_csv('vehicles_us.csv')
+import plotly.express as px
 
 st.header('Compare price distribution between manufacturers')
+
+df = pd.read_csv('vehicles_us.csv')
+days_price = px.scatter(df, x='days_listed', y='price', title='Days listed vs price of vehicle', labels={'x': 'days listed', 'y': 'Price $'}, opacity=0.7)
+
+st.write(days_price)
+
+
 
