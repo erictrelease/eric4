d3d3385240c9ed32f316db893614e08ef1005f74
erictrelease <erictrelease@gmail.com>
2024-03-02 15:47:20 -0500

    march 2 4

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import plotly.express as px
 
-st.header('Compare price distribution between manufacturers')
+st.header('Investigating Vehicle sales in the US')
 
 df = pd.read_csv('vehicles_us.csv')
 days_price = px.scatter(df, x='days_listed', y='price', title='Days listed vs price of vehicle', labels={'x': 'days listed', 'y': 'Price $'}, opacity=0.7)
@@ -33,6 +33,16 @@ odo_plot = px.scatter(odo_df, x='odometer', y='price', title='Odometer vs price 
 odo_plot.show()
 
 st.write(odo_plot)
+
+# Explore relationship between condition and days listed
+days_to_sell_condition = df.groupby('condition')['days_listed'].mean().round(1)
+days_to_sell_condition = pd.DataFrame(days_to_sell_condition).reset_index()
+print(days_to_sell_condition)
+
+st.header('Vehicles listed as "new" sold 2 days sooner on average.')
+dc_bar = px.bar(days_to_sell_condition, x='condition', y='days_listed', title='How many days to sell based on car condition')
+dc_bar.update_layout(yaxis=dict(range=[30, 40]))
+dc_bar.show()
 
 
 
