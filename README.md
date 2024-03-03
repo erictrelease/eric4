1b336690e6becaaa70be865841d2cfaaf0da1d50
erictrelease <erictrelease@gmail.com>
2024-03-02 19:05:49 -0500

    march 2 8

--- README.md
+++ README.md
@@ -1,5 +1,8 @@
 # eric4
-This project investigates vehicle advertisement data in the United States from May 2018 until April 2019.
-Exploratory analysis revealed the following relationships:
-- vehicles listed as new sold on average 2 days sooner.
+This project investigates vehicle advertisement data in the United States from May 2018 until April 2019.  Exploratory data analysis was done to see how sale price related to days listed, odometer reading, type of vehicle, and condition of the vehicle.  Scatterplots, histograms, and barplots were created when appropriate.  These results were published to https://eric4.onrender.com/ where the user can select which parameters to investigate.
+
+Based on the EDA the following observations were made:
+1. vehicles listed as "new" sold on average 2 days sooner.
+2. there is an inverse relationship between sale price and miles on odometer.
+3. there is an inverse relationship between sale price and days the ad was listed.
 
--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import plotly.express as px
 
-st.header('Investigating Vehicle sales in the US')
+st.title('Investigating how parameters affect the sale price for vehicles sold in the US between May 2018 and April 2014')
 
 df = pd.read_csv('vehicles_us.csv')
 
@@ -19,7 +19,7 @@ days_price = px.scatter(days_price_df, x='days_listed', y='price', title='Days l
 odo_df = df.dropna(subset=['odometer'])
 # Remove outliers for odometer above 400,000
 odo_df = odo_df[odo_df['odometer']<400000]
-# Remove outliers for price over $100k
+# Remove outliers for price over $60k
 odo_df = odo_df[odo_df['price']<60000]
 odo_df.info()
 odo_plot = px.scatter(odo_df, x='odometer', y='price', title='Odometer vs price of vehicle', labels={'x': 'Odometer', 'y': 'Price $'}, opacity=0.7)
@@ -27,10 +27,9 @@ odo_plot = px.scatter(odo_df, x='odometer', y='price', title='Odometer vs price 
 # Explore relationship between condition and days listed
 days_to_sell_condition = df.groupby('condition')['days_listed'].mean().round(1)
 days_to_sell_condition = pd.DataFrame(days_to_sell_condition).reset_index()
-dc_bar = px.bar(days_to_sell_condition, x='condition', y='days_listed', title='How many days to sell based on car condition')
+Colors = ['red', 'blue', 'green', 'yellow', 'pink', 'purple']
+dc_bar = px.bar(days_to_sell_condition, x='condition', y='days_listed', color=Colors, title='How many days to sell based on car condition')
 dc_bar.update_layout(yaxis=dict(range=[30, 40]))
-
-
 
 # Explore df based on price and type of vehicle
 # Remove outliers based on price
@@ -38,15 +37,14 @@ price_typeh = df[df['price']<50000]
 # Histogram for price and type of vehicles
 pt_hist = px.histogram(price_typeh, x='price', color = 'type', title='Price of vehicle based on type', nbins=50, opacity=0.7)
 
-
 # Explore df based on price and condition of vehicle
 # Remove outliers based on price
 pc = df[df['price']<50000]
 # Histogram for price and type of vehicles
 pc_hist = px.histogram(pc, x='price', color = 'condition', title='Price of vehicle based on condition', nbins=50, opacity=0.7)
 
-
-st.title('Select one or more of the following parameters to see how it affects vehicle price:')
+# Checkboxes
+st.header('Select one or more of the following parameters to see how it affects vehicle price:')
 check = st.checkbox('Days Listed')
 check1 = st.checkbox('Miles on Odometer')
 check2 = st.checkbox('Car Condition')
@@ -62,6 +60,8 @@ if check1:
 
 if check2:
     st.write(pc_hist)
+    st.write(dc_bar)
+    st.write('Vehicles listed as "new" sold 2 days sooner on average.')
 
 if check3:
     st.write(pt_hist)
